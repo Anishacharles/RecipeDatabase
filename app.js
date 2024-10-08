7bfab656358f4c430c012cce6a887efb579ad758
Anishacharles <anishacharles03@gmail.com>
2024-10-08 15:39:13 +0530

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ connectDB();
 //routes
 app.use("/api/recipes", recipeRoutes);
 
-const PORT = process.env.PORT || 5500 ;
+const PORT = process.env.PORT || 6500 ;
 
 app.listen(PORT, () =>{
     console.log(`server is running at the url http://localhost:${PORT}`)
